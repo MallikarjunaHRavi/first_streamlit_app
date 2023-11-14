fb771ac4646e4d77702f379c687e9e87360abc95
MALLIKARJUNA HOSAHALLI RAVI <135298294+MallikarjunaHRavi@users.noreply.github.com>
2023-11-14 12:51:50 -0500

    Update streamlit_app.py
    added the emoji and new item

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
 streamlit.title('My Parents New Healthy Dinner')
-
+ 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kayle, Spinach and Rocket Smoothie')
-streamlit.text('Half- boiled cage free egg 2')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text(' 🥗 Kayle, Spinach and Rocket Smoothie')
+streamlit.text('🐔 Half- boiled cage free egg 2')
+streamlit.text('🥑🍞 Avacado Toast')
