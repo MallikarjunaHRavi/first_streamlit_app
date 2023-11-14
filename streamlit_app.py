f0b2f679d783045742fa63c5eb6472d8f6442fa1
MALLIKARJUNA HOSAHALLI RAVI <135298294+MallikarjunaHRavi@users.noreply.github.com>
2023-11-14 13:15:05 -0500

    Update streamlit_app.py
    spelling errors from the fruit picks

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,11 +14,11 @@ my_fruit_list=pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/
 my_fruit_list= my_fruit_list.set_index('Fruit')
 
 # let us put list here so they can pick the fruits of their choice
-streamlit.multiselect('pick some fruits:', list(my_fruit_list.index))
+streamlit.multiselect("pick some fruits:", list(my_fruit_list.index))
 
 # display the table on the page 
 streamlit.dataframe(my_fruit_list)
 
 # pick the list here so they can pick the fruit they want 
-streamlit.multiselect('pick some fruits:', list(my_fruit_list.index),['Avacado','Strawberries'])
+streamlit.multiselect("pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 
