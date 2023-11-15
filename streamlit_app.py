cc711ba2f8bab778e29e0d09a8e04af435390986
MALLIKARJUNA HOSAHALLI RAVI <135298294+MallikarjunaHRavi@users.noreply.github.com>
2023-11-15 17:43:54 -0500

    Update streamlit_app.py
    adding the json format to the fruits list

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,10 +23,12 @@ fruits_to_show= my_fruit_list.loc[fruits_selected]
 # display the table on the page 
 streamlit.dataframe(fruits_to_show)
 
+#new section to display
+strwamlit.header('Fruityvice Fruit Advice!')
 #importing the request from api 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
 
 
 
