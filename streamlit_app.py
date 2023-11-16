9e6bc852057c926947f8a7b7a97576da720f61de
MALLIKARJUNA HOSAHALLI RAVI <135298294+MallikarjunaHRavi@users.noreply.github.com>
2023-11-16 02:05:22 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 streamlit.title('My Parents New Healthy Dinner')
- streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text(' 🥗 Kayle, Spinach and Rocket Smoothie')
 streamlit.text('🐔 Half- boiled cage free egg 2')
